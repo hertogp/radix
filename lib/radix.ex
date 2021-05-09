466a7a4b061f091f14e18a101c424bc1f508526e
hertogp <git.hertogp@gmail.com>
2021-05-09 12:28:27 +0200

    reduce instead of exec

--- lib/radix.ex
+++ lib/radix.ex
@@ -183,49 +183,6 @@ defmodule Radix do
     end
   end
 
-  # get a {k,v}-pair from a leaf
-  @spec getp(leaf, key, any) :: {key, value} | any
-  defp getp(nil, _, default), do: default
-
-  defp getp(leaf, key, default) do
-    case List.keyfind(leaf, key, 0) do
-      nil -> default
-      keyval -> keyval
-    end
-  end
-
-  # delete a {k,v}-pair from the tree
-  # @spec delp(tree | leaf, list(key) | key) :: tree
-  # defp delp(tree, keys) when is_list(keys),
-  #   do: Enum.reduce(keys, tree, fn k, t -> delp(t, k) end)
-
-  @spec delp(tree | leaf, key) :: tree | leaf
-  defp delp({bit, l, r}, key) do
-    case bit(key, bit) do
-      0 -> delp({bit, delp(l, key), r})
-      1 -> delp({bit, l, delp(r, key)})
-    end
-  end
-
-  # key wasn't in the tree
-  defp delp(nil, _key),
-    do: nil
-
-  # key leads to leaf
-  defp delp(leaf, key) do
-    case List.keydelete(leaf, key, 0) do
-      [] -> nil
-      leaf -> leaf
-    end
-  end
-
-  # always keep the root, eliminate empty nodes and promote half-empty nodes
-  defp delp({0, l, r}), do: {0, l, r}
-  defp delp({_, nil, nil}), do: nil
-  defp delp({_, l, nil}), do: l
-  defp delp({_, nil, r}), do: r
-  defp delp({bit, l, r}), do: {bit, l, r}
-
   # API
 
   @doc """
@@ -280,9 +237,13 @@ defmodule Radix do
   """
   @spec get(tree, key, any) :: {key, value} | any
   def get({0, _, _} = tree, key, default \\ nil) when is_bitstring(key) do
-    tree
-    |> leaf(key)
-    |> getp(key, default)
+    # tree
+    # |> leaf(key)
+    # |> getp(key, default)
+    case leaf(tree, key) do
+      nil -> default
+      leaf -> List.keyfind(leaf, key, 0, default)
+    end
   end
 
   @doc """
@@ -400,6 +361,34 @@ defmodule Radix do
   @spec delete(tree, key) :: tree
   def delete({0, _, _} = tree, key) when is_bitstring(key),
     do: delp(tree, key)
+
+  # delete a {k,v}-pair from the tree
+  @spec delp(tree | leaf, key) :: tree | leaf
+  defp delp({bit, l, r}, key) do
+    case bit(key, bit) do
+      0 -> delp({bit, delp(l, key), r})
+      1 -> delp({bit, l, delp(r, key)})
+    end
+  end
+
+  # key wasn't in the tree
+  defp delp(nil, _key),
+    do: nil
+
+  # key leads to leaf
+  defp delp(leaf, key) do
+    case List.keydelete(leaf, key, 0) do
+      [] -> nil
+      leaf -> leaf
+    end
+  end
+
+  # always keep the root, eliminate empty nodes and promote half-empty nodes
+  defp delp({0, l, r}), do: {0, l, r}
+  defp delp({_, nil, nil}), do: nil
+  defp delp({_, l, nil}), do: l
+  defp delp({_, nil, r}), do: r
+  defp delp({bit, l, r}), do: {bit, l, r}
 
   @doc """
   Drops the given `keys` from the radix `tree` using an exact match.
@@ -526,6 +515,7 @@ defmodule Radix do
   def search({0, _, _} = tree, key, :less) when is_bitstring(key),
     do: apm(tree, key)
 
+  # all prefix matches: search key is prefix of stored key(s)
   @spec apm(tree | leaf, key) :: [{key, value}]
   defp apm({b, l, r} = _tree, key) do
     case bit(key, b) do
@@ -540,6 +530,7 @@ defmodule Radix do
   defp apm(leaf, key),
     do: Enum.filter(leaf, fn {k, _} -> is_prefix?(k, key) end)
 
+  # all reverse prefix matches: stored key is prefix of search key
   @spec rpm(tree | leaf, key) :: [{key, value}]
   defp rpm({b, l, r} = _tree, key) when bit_size(key) < b do
     rpm(r, key) ++ rpm(l, key)
@@ -589,10 +580,14 @@ defmodule Radix do
   defp to_list(leaf, acc), do: acc ++ leaf
 
   @doc """
-  Executes *fun* on all key,value-pairs in the radix tree in a depth-first fashion.
+  Invokes *fun* for each key,value-pair in the radix tree with the accumulator.
 
-  *fun*'s signature is ({`t:key/0`, `t:value/0`}, `t:acc/0`) -> `t:acc/0`, where
-  the caller supplies both *fun* and *acc*.
+  The initial value of the accumulator is `acc`. The function is invoked for
+  each key,value-pair in the radix tree with the accumulator in a depth-first
+  fashion. The result returned by the function is used as the accumulator for
+  the next iteration.  The function returns the last accumulator.
+
+  *fun*'s signature is (`t:key/0`, `t:value/0`, `t:acc/0`) -> `t:acc/0`.
 
   ## Example
 
@@ -605,27 +600,31 @@ defmodule Radix do
       iex>
       iex> # get values
       iex>
-      iex> f = fn {_key, value}, acc -> [value | acc] end
-      iex> exec(t, f, []) |> Enum.reverse()
+      iex> f = fn _key, value, acc -> [value | acc] end
+      iex> reduce(t, f, []) |> Enum.reverse()
       ["1.1.1.0/25", "1.1.1.0/24", "1.1.1.128/25", "3.0.0.0/8"]
 
   """
-  @spec exec(tree, ({key, value}, acc -> acc), acc) :: acc
-  def exec(tree, fun, acc)
-  def exec(nil, _f, acc), do: acc
-  def exec([], _f, acc), do: acc
-  def exec({_, l, r}, fun, acc), do: exec(r, fun, exec(l, fun, acc))
-  def exec([{k, v} | tail], fun, acc), do: exec(tail, fun, fun.({k, v}, acc))
+  @spec reduce(tree, (key, value, acc -> acc), acc) :: acc
+  def reduce({0, _, _} = tree, fun, acc) when is_function(fun, 3),
+    do: reducep(tree, fun, acc)
+
+  @spec reducep(tree, (key, value, acc -> acc), acc) :: acc
+  def reducep(tree, fun, acc)
+  def reducep(nil, _f, acc), do: acc
+  def reducep([], _f, acc), do: acc
+  def reducep({_, l, r}, fun, acc), do: reducep(r, fun, reducep(l, fun, acc))
+  def reducep([{k, v} | tail], fun, acc), do: reducep(tail, fun, fun.(k, v, acc))
 
   @doc """
-  Execute *fun* on all (internal and leaf) nodes of the radix tree using either
+  Invokes *fun* on all (internal and leaf) nodes of the radix tree using either
   `:inorder`, `:preorder` or `:postorder` traversal.
 
   *fun* should have the signatures:
   -  (`t:acc/0`, `t:tree/0`) -> `t:acc/0`
   -  (`t:acc/0`, `t:leaf/0`) -> `t:acc/0`
 
-  and where *leaf* might be nil.
+  Note that *leaf* might be nil.
 
   ## Example
 
